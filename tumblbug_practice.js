5eeae31542d52a47c9558fd2dabf29bbff3a3734
5uhwann <5uhwann27@gmail.com>
2022-06-28 22:21:35 +0900

    add category

---
+++ tumblbug_practice.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+  let list_category = document.getElementById("category_list");
+
+  let category_list = document.getElementById("category");
+
+  list_category.addEventListener("mouseover", function () {
+    category_list.style.display = "inline";
+  });
+
+  category_list.addEventListener("mouseover", function () {
+    category_list.style.display = "inline";
+  });
+
+  category_list.addEventListener("mouseout", function () {
+    category_list.style.display = "none";
+  });
+};
